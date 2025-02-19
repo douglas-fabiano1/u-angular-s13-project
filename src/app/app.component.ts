cd600315a1914e206b10397bd96159f58245bf1e
Douglas <contatodouglasfabiano1@gmail.com>
2025-02-19 19:07:16 -0300

    brazilian states service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from './services/users.service';
 import { GenresService } from './services/genres.service';
+import { BrazilianStatesService } from './services/brazilian-states.service';
 
 @Component({
   selector: 'app-root',
@@ -10,15 +11,26 @@ import { GenresService } from './services/genres.service';
 export class AppComponent implements OnInit {
   usersList: any = [];
   genresList: any = [];
+  statesList: any = [];
 
   constructor(
     private readonly _usersService: UsersService,
-    private readonly _genresService: GenresService
+    private readonly _genresService: GenresService,
+    private readonly _brazilianStatesService: BrazilianStatesService
   ) {}
 
   ngOnInit() {
     this.getUsers();
     this.getGenres();
+    this.getStates();
+  }
+
+  private getStates() {
+    this._brazilianStatesService.getStates().subscribe((statesListResponse) => {
+      this.statesList = statesListResponse;
+
+      console.log(this.statesList);
+    });
   }
 
   private getGenres() {
