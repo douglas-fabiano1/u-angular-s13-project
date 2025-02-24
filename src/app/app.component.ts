e9ca7f5f103d5e4412326c95dcd17a818e0dd60f
Douglas Fabiano <contatodouglasfabiano1@gmail.com>
2025-02-24 10:41:45 -0300

    user logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { BrazilianStatesService } from './services/brazilian-states.service';
 import { UsersListResponse } from './types/users-list-response';
 import { GenresListResponse } from './types/genres-list-response';
 import { StatesListResponse } from './types/states-list-response';
+import { IUser } from './interfaces/user/user.interface';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +13,9 @@ import { StatesListResponse } from './types/states-list-response';
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
+  userSelected: IUser = {} as IUser;
+  userSelectedIndex: number | undefined;
+
   usersList: UsersListResponse = [];
   genresList: GenresListResponse = [];
   statesList: StatesListResponse = [];
@@ -28,11 +32,18 @@ export class AppComponent implements OnInit {
     this.getStates();
   }
 
+  onUserSelected(userIndex: number) {
+    const userFound = this.usersList[userIndex];
+
+    if (userFound) {
+      this.userSelectedIndex = userIndex;
+      this.userSelected = structuredClone(userFound);
+    }
+  }
+
   private getStates() {
     this._brazilianStatesService.getStates().subscribe((statesListResponse) => {
       this.statesList = statesListResponse;
-
-      console.log(this.statesList);
     });
   }
 
