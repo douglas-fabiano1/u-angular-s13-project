547803b1e0a2c1e70407cfb0e4b039b25d731f79
Douglas <contatodouglasfabiano1@gmail.com>
2025-02-25 11:56:17 -0300

    credentials validator directive - username and email

--- src/app/services/users-placeholder.service.ts
+++ src/app/services/users-placeholder.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { UsersPlaceholderListResponse } from "../types/users-placeholder-list-response";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UsersPlaceholderListResponse } from '../types/users-placeholder-list-response';
 
 @Injectable({
   providedIn: 'root',
@@ -10,12 +10,12 @@ export class UsersPlaceholderService {
   // o HttpClient vai disponibilizar os métodos get, post, put, delete, e vai fazer a chamada http.
   // Não vamos mockar uma lista, vamos trazê-las a partir do servidor jsonplaceholder.typicode.com
   // devo fazer o import do HttpClientModule no app.module.ts para que eu possa utilizar o HttpClient
-  constructor(
-    private readonly _httpClient: HttpClient
-  ) { }
+  constructor(private readonly _httpClient: HttpClient) {}
 
   // tipar de acordo com o que é retornado do endpoint
   getUsersPlaceholder(): Observable<UsersPlaceholderListResponse> {
-    return this._httpClient.get<UsersPlaceholderListResponse>('https://jsonplaceholder.typicode.com/users');
+    return this._httpClient.get<UsersPlaceholderListResponse>(
+      'https://jsonplaceholder.typicode.com/users'
+    );
   }
 }
