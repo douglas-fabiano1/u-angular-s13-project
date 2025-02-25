6daeb0b5ca638440d73c8ea5b6eb949c39eb59d1
Douglas <contatodouglasfabiano1@gmail.com>
2025-02-25 17:38:12 -0300

    password input

---
+++ src/app/directives/password-strength-validator.directive.ts
@@ -0,0 +1,36 @@
+import { Directive } from '@angular/core';
+import {
+  AbstractControl,
+  NG_VALIDATORS,
+  ValidationErrors,
+  Validator,
+} from '@angular/forms';
+
+import zxcvbn from 'zxcvbn';
+
+// validador síncrono
+@Directive({
+  selector: '[appPasswordStrengthValidator]',
+  providers: [
+    {
+      provide: NG_VALIDATORS,
+      useExisting: PasswordStrengthValidatorDirective,
+      multi: true,
+    },
+  ],
+})
+export class PasswordStrengthValidatorDirective implements Validator {
+  validate(control: AbstractControl<any, any>): ValidationErrors | null {
+    if (!control || !control.value) return null;
+
+    const result = zxcvbn(control.value);
+
+    const PASSWORD_IS_WAK_OR_MEDIUM = result.score !== 4;
+
+    if (PASSWORD_IS_WAK_OR_MEDIUM) {
+      return { invalidPasswordStrength: true };
+    }
+
+    return null;
+  }
+}
