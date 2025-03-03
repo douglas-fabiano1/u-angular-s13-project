8373707613ec6a521b0845af60bd32b343ac8df2
Douglas Fabiano <contatodouglasfabiano1@gmail.com>
2025-03-03 18:21:27 -0300

    data from component to dialog

--- src/app/components/user-before-and-after-dialog/user-before-and-after-dialog.component.ts
+++ src/app/components/user-before-and-after-dialog/user-before-and-after-dialog.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { IUser } from '../../interfaces/user/user.interface';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-user-before-and-after-dialog',
@@ -6,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './user-before-and-after-dialog.component.scss'
 })
 export class UserBeforeAndAfterDialogComponent {
-
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: { originalUser: IUser, updatedUser: IUser, }
+  ) { }
 }
